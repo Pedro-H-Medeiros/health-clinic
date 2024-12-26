68e55663dc608999c75450852690d1c30a0381a0
Pedro-H-Medeiros <medeirosprogrammer@gmail.com>
2024-12-26 11:07:09 -0300

    feat: handle error input

--- src/pages/dashboard/appointment.tsx
+++ src/pages/dashboard/appointment.tsx
@@ -1,4 +1,14 @@
+import { useForm } from 'react-hook-form'
+
 export function Appointment() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm()
+
+  function handleSubmitMessage() {}
+
   return (
     <div
       id="appointment"
@@ -26,20 +36,27 @@ export function Appointment() {
                   </p>
                 </div>
 
-                <form className="space-y-4">
+                <form
+                  onSubmit={handleSubmit(handleSubmitMessage)}
+                  className="space-y-4"
+                >
                   <div className="space-y-3">
                     <label
                       htmlFor="name"
                       className="block text-base font-thin text-gray-700 md:text-xl"
                     >
-                      Name
+                      Name <span className="text-red-500">*</span>
                     </label>
                     <input
                       type="text"
                       id="name"
-                      className="h-12 w-full rounded-full border border-gray-400 px-5 text-2xl outline-none focus:border-red-500 focus:ring-red-500 md:h-16"
-                      required
+                      className="h-12 w-full rounded-full border border-gray-400 px-5 text-xl outline-none focus:border-red-500 focus:ring-red-500 md:h-16"
+                      {...register('name', { required: true })}
+                      aria-invalid={errors.name ? 'true' : 'false'}
                     />
+                    {errors.name?.type === 'required' && (
+                      <p className="text-red-500">* Nome não pode ser vazio</p>
+                    )}
                   </div>
 
                   <div className="space-y-3">
@@ -47,28 +64,37 @@ export function Appointment() {
                       htmlFor="email"
                       className="block text-base font-thin text-gray-700 md:text-xl"
                     >
-                      Email
+                      Email <span className="text-red-500">*</span>
                     </label>
                     <input
                       type="email"
                       id="email"
-                      className="h-12 w-full rounded-full border border-gray-400 px-5 text-2xl outline-none focus:border-red-500 focus:ring-red-500 md:h-16"
-                      required
+                      className="h-12 w-full rounded-full border border-gray-400 px-5 text-xl outline-none focus:border-red-500 focus:ring-red-500 md:h-16"
+                      {...register('email', { required: true })}
+                      aria-invalid={errors.email ? 'true' : 'false'}
                     />
+                    {errors.name?.type === 'required' && (
+                      <p className="text-red-500">* Email não pode ser vazio</p>
+                    )}
                   </div>
 
-                  <div>
+                  <div className="space-y-3">
                     <label
                       htmlFor="message"
                       className="block text-base font-thin text-gray-700 md:text-xl"
                     >
-                      Message
+                      Message <span className="text-red-500">*</span>
                     </label>
                     <textarea
                       id="message"
                       rows={4}
-                      className="w-full rounded-xl border border-gray-400 px-5 text-2xl outline-none focus:border-red-500 focus:ring-red-500"
-                    ></textarea>
+                      className="w-full rounded-xl border border-gray-400 px-5 text-xl outline-none focus:border-red-500 focus:ring-red-500"
+                      {...register('message', { required: true })}
+                      aria-invalid={errors.email ? 'true' : 'false'}
+                    />
+                    {errors.message?.type === 'required' && (
+                      <p className="text-red-500">* Escreva uma mensagem</p>
+                    )}
                   </div>
 
                   <button
